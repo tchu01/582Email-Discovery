4a4cc7fcf6f7f6defc00eda309b22fcc429a33ae
Sean Bayley <seanbayley@gmail.com>
2016-11-04 13:41:07 -0700

    no message

--- src/main.py
+++ src/main.py
@@ -1,38 +1,41 @@
-import sys, os, nltk
+import nltk
 import mailbox
-
 from os import listdir
+from os.path import join
 from scrape_mbox import scrape, path_to_takeout1, path_to_takeout2
 
-def popular_ngrams(candidate_dict):
-   top_trigrams = []
-   top_fourgrams = []
-   top_fivegrams = []
-   for key,value in candidate_dict.items():
-      if candidate_dict[key]['word_tokens'] is not None:
-         trigrams = nltk.trigrams(candidate_dict[key]['word_tokens'])
-         fdist = nltk.FreqDist(trigrams)
-         print(fdist.most_common(5))
 
-         fourgrams = nltk.ngrams(candidate_dict[key]['word_tokens'], 4)
-         fdist = nltk.FreqDist(fourgrams)
-         print(fdist.most_common(5))
+def popular_ngrams(candidate_dict, cand=None):
+    if not cand:
+        cand = 'Unknown'
+    top_trigrams = []
+    top_fourgrams = []
+    top_fivegrams = []
+    for date, email in candidate_dict.items():
+        if email['word_tokens']:
+            trigrams = nltk.trigrams(email['word_tokens'])
+            top_trigrams.extend(map(lambda x: x[0], nltk.FreqDist(trigrams).most_common(5)))
 
-         fivegrams = nltk.ngrams(candidate_dict[key]['word_tokens'], 5)
-         fdist = nltk.FreqDist(fivegrams)
-         print(fdist.most_common(5))
-      else:
-         print("Email had no payload")
+            fourgrams = nltk.ngrams(email['word_tokens'], 4)
+            top_fourgrams.extend(map(lambda x: x[0], nltk.FreqDist(fourgrams).most_common(5)))
+
+            fivegrams = nltk.ngrams(email['word_tokens'], 5)
+            top_fivegrams.extend(map(lambda x: x[0], nltk.FreqDist(fivegrams).most_common(5)))
+
+        else:
+            print("Email had no payload")
+
+    print(cand)
+    print('trigrams: {}'.format(', '.join(map(str, top_trigrams))))
+
 
 if __name__ == '__main__':
-   takeout1_mboxes = [path_to_takeout1 + "/" + f for f in listdir(path_to_takeout1)]
-   takeout2_mboxes = [path_to_takeout2 + "/" + f for f in listdir(path_to_takeout2)]
+    takeout1_mboxes = [(join(path_to_takeout1, f), f.split('.')[0].split('-')[-1]) for f in listdir(path_to_takeout1)]
+    takeout2_mboxes = [(join(path_to_takeout2, f), f.split('.')[0].split('-')[-1]) for f in listdir(path_to_takeout2)]
 
-   for mbox in takeout1_mboxes:
-      print(mbox)
-      candidate_dict = scrape(mbox)
-      popular_ngrams(candidate_dict)
-      break
+    for mbox, cand in takeout1_mboxes[:1]:
+        candidate_dict = scrape(mbox)
+        popular_ngrams(candidate_dict, cand)
 
-   #for m in takeout2_mboxes:
-   #   print(m)
+        # for m in takeout2_mboxes:
+        #   print(m)
--- src/scrape_mbox.py
+++ src/scrape_mbox.py
@@ -1,62 +1,62 @@
 import os, sys, nltk
 import mailbox
-
 from os import listdir
 from collections import defaultdict
 
 path_to_takeout1 = os.path.abspath("../data/Takeout1/Mail")
 path_to_takeout2 = os.path.abspath("../data/Takeout2/Mail")
 
+
 def scrape(mbox_filename):
-   candidate_dict = {}
-   mbox = mailbox.mbox(mbox_filename)
+    candidate_dict = {}
+    mbox = mailbox.mbox(mbox_filename)
 
-   for message in mbox:
-      date = message['date']
-      subject = message['subject']
-      payload = message.get_payload()      
-      text_payload = get_body(message)
+    for message in mbox:
+        date = message['date']
+        subject = message['subject']
+        payload = message.get_payload()
+        text_payload = get_body(message)
 
-      candidate_dict[date] = {}
-      candidate_dict[date]['subject'] = subject
-      candidate_dict[date]['payload'] = text_payload
-      if text_payload is not None:
-         candidate_dict[date]['word_tokens'] = nltk.word_tokenize(text_payload)
-      else:
-         candidate_dict[date]['word_tokens'] = None 
+        candidate_dict[date] = {}
+        candidate_dict[date]['subject'] = subject
+        candidate_dict[date]['payload'] = text_payload
+        if text_payload:
+            candidate_dict[date]['word_tokens'] = nltk.word_tokenize(text_payload)
+        else:
+            candidate_dict[date]['word_tokens'] = None
 
-      #print("Date: " + str(date))
-      #print("Subject: " + str(subject))
-      #print("Payload: " + str(text_payload))
-      #print("Tokens: " + str(nltk.word_tokenize(text_payload)))
-      #print("Bigrams: " + str(list(nltk.bigrams(candidate_dict[date]['word_tokens']))))
-      #break
+        # print("Date: " + str(date))
+        # print("Subject: " + str(subject))
+        # print("Payload: " + str(text_payload))
+        # print("Tokens: " + str(nltk.word_tokenize(text_payload)))
+        # print("Bigrams: " + str(list(nltk.bigrams(candidate_dict[date]['word_tokens']))))
 
-   #print(dict(candidate_dict))
-   return candidate_dict
+    return candidate_dict
 
-def get_body(message): 
-   body = None
-   if message.is_multipart():
-      for part in message.walk():
-         if part.is_multipart():
-            for subpart in part.walk():
-               if subpart.get_content_type() == 'text/plain':
-                  body = subpart.get_payload(decode=True)
-         elif part.get_content_type() == 'text/plain':
-            body = part.get_payload(decode=True)
-   elif message.get_content_type() == 'text/plain':
-      body = message.get_payload(decode=True)
 
-   if body is not None:
-      #print("PAYLOAD: " + str(body.decode('UTF-8')))
-      return body.decode('UTF-8', errors='replace')
-   else:
-      return None
+def get_body(message):
+    body = None
+    if message.is_multipart():
+        for part in message.walk():
+            if part.is_multipart():
+                for subpart in part.walk():
+                    if subpart.get_content_type() == 'text/plain':
+                        body = subpart.get_payload(decode=True)
+            elif part.get_content_type() == 'text/plain':
+                body = part.get_payload(decode=True)
+    elif message.get_content_type() == 'text/plain':
+        body = message.get_payload(decode=True)
+
+    if body is not None:
+        # print("PAYLOAD: " + str(body.decode('UTF-8')))
+        return body.decode('UTF-8', errors='replace')
+    else:
+        return None
+
 
 if __name__ == '__main__':
-   takeout1_mboxes = [path_to_takeout1 + "/" + f for f in listdir(path_to_takeout1)]
-   takeout2_mboxes = [path_to_takeout2 + "/" + f for f in listdir(path_to_takeout2)]
-   
-   #print("Looking at mbox: " + str(takeout1_mboxes[0]))
-   scrape(takeout1_mboxes[0])
+    takeout1_mboxes = [path_to_takeout1 + "/" + f for f in listdir(path_to_takeout1)]
+    takeout2_mboxes = [path_to_takeout2 + "/" + f for f in listdir(path_to_takeout2)]
+
+    # print("Looking at mbox: " + str(takeout1_mboxes[0]))
+    scrape(takeout1_mboxes[0])
