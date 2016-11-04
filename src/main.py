e9087198e3bc75eda21dedf3c31c7097e48c6817
Timothy Chu <tchu01@calpoly.edu>
2016-11-04 14:54:01 -0700

    Adding potential new interesting thing: talk about money. Tinkered with ngram models, still needs to be completed.

--- src/main.py
+++ src/main.py
@@ -22,15 +22,35 @@ def popular_ngrams(candidate_dict, cand=None):
 
     print('Candidate: {}'.format(cand))
     print('trigrams: {}'.format(', '.join(map(str, map(lambda x: x[0], nltk.FreqDist(trigrams).most_common(5))))))
+    # print('fourgrams: {}'.format(', '.join(map(str, map(lambda x: x[0], nltk.FreqDist(fourgrams).most_common(5))))))
+    # print('fivegrams: {}'.format(', '.join(map(str, map(lambda x: x[0], nltk.FreqDist(fivegrams).most_common(5))))))
 
+def money_talk(candidate_dict, cand=None):
+    if not cand:
+        cand = 'Unknown'
+    sents = []
+    for date, email in candidate_dict.items():
+        if email['sent_tokens']:
+            for sent in email['sent_tokens']:
+                if '$' in sent or 'money' in sent or 'donate' in sent:
+                    sents.append(sent)
+
+    print('Candidate: {}'.format(cand))
+    print(sents[:2])
 
 if __name__ == '__main__':
     takeout1_mboxes = [(join(path_to_takeout1, f), f.split('.')[0].split('-')[-1]) for f in listdir(path_to_takeout1)]
     takeout2_mboxes = [(join(path_to_takeout2, f), f.split('.')[0].split('-')[-1]) for f in listdir(path_to_takeout2)]
 
-    for mbox, cand in takeout1_mboxes[:1]:
+    for mbox, cand in takeout1_mboxes:
         candidate_dict = scrape(mbox)
+        # Interesting thing 1
         popular_ngrams(candidate_dict, cand)
 
-        # for m in takeout2_mboxes:
-        #   print(m)
+        # Interesting thing 2
+        money_talk(candidate_dict, cand) 
+
+        # Interesting thing 3
+
+    # for m in takeout2_mboxes:
+    #   print(m)
--- src/scrape_mbox.py
+++ src/scrape_mbox.py
@@ -1,11 +1,10 @@
-import os, sys, nltk
+import os, sys, nltk, re
 import mailbox
 from os import listdir
 from collections import defaultdict
 
 path_to_takeout1 = os.path.abspath("../data/Takeout1/Mail")
 path_to_takeout2 = os.path.abspath("../data/Takeout2/Mail")
-
 
 def scrape(mbox_filename):
     candidate_dict = {}
@@ -15,24 +14,27 @@ def scrape(mbox_filename):
         date = message['date']
         subject = message['subject']
         payload = message.get_payload()
-        text_payload = get_body(message)
+        text_payload = remove_links(get_body(message))
 
         candidate_dict[date] = {}
         candidate_dict[date]['subject'] = subject
         candidate_dict[date]['payload'] = text_payload
+
         if text_payload:
             candidate_dict[date]['word_tokens'] = nltk.word_tokenize(text_payload)
+            candidate_dict[date]['sent_tokens'] = nltk.sent_tokenize(text_payload)
         else:
             candidate_dict[date]['word_tokens'] = None
+            candidate_dict[date]['sent_tokens'] = None
 
         # print("Date: " + str(date))
         # print("Subject: " + str(subject))
-        # print("Payload: " + str(text_payload))
+        # print("Payload: " + str(candidate_dict[date]['without_links']))
         # print("Tokens: " + str(nltk.word_tokenize(text_payload)))
         # print("Bigrams: " + str(list(nltk.bigrams(candidate_dict[date]['word_tokens']))))
+        # break
 
     return candidate_dict
-
 
 def get_body(message):
     body = None
@@ -53,6 +55,13 @@ def get_body(message):
     else:
         return None
 
+def remove_links(payload):
+    if payload is not None:
+        temp = re.sub("(<(.*)>)", "", payload)
+        temp = re.sub("((http[s]?|ftp):\/)?\/?([^:\/\s]+)((\/\w+)*\/)([\w\-\.]+[^#?\s]+)(.*)?(#[\w\-]+)?", "", temp).replace("<>", "")
+        return temp
+    else:
+        return payload
 
 if __name__ == '__main__':
     takeout1_mboxes = [path_to_takeout1 + "/" + f for f in listdir(path_to_takeout1)]
